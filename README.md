ed573b8e1b50a6d90631ccc674f4363ec8322fdb
velioterio <velioterio@dxc.com>
2019-09-17 15:55:05 -0300

    Subindo projeto de referência com Swagger

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ProjSwagger
 Projeto de referência com Swagger para documentação
+
+Utilizar essa url no navegador para visualizar a interface gráfica da documentação pelo Swagger:
+http://localhost:8080/swagger-ui.html
---
+++ src/main/java/com/kazale/api/ProjSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSwaggerApplication.class, args);
+	}
+}
